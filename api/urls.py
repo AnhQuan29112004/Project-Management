35d26d1be25a32a0c2a3ce0fc4bb6953c8801741
AnhQuan29112004 <namcaodidong2004@gmail.com>
2025-04-10 00:34:44 +0700

    update

--- api/urls.py
+++ api/urls.py
@@ -7,14 +7,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-version_api = config('VERSION_API')
-from Account.views import RegisterAPI, LogoutAPI, GetUserView,LoginAPI
+from Account.views import RegisterAPI, LogoutAPI, GetUserView,LoginAPI, CustomTokenRefreshView
 version_api = config('VERSION_API')
 
 urlpatterns = [
     path(f'{version_api}/auth/login/', LoginAPI.as_view(), name='login'),
     path(f'{version_api}/auth/register/', RegisterAPI.as_view(), name='register'),
     path(f'{version_api}/auth/getUser/', GetUserView.as_view(), name='getUser'),
-    path(f'{version_api}/auth/getAccesstoken/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(f'{version_api}/auth/getAccesstoken/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path(f'{version_api}/auth/logout/', LogoutAPI.as_view(), name='logout'),
 ]
