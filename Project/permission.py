58b92d0dbcc0d0cecb611cf6d8785d1ddf58ad8e
AnhQuan29112004 <namcaodidong2004@gmail.com>
2025-04-12 00:10:50 +0700

    add class to get perm

--- Project/permission.py
+++ Project/permission.py
@@ -8,11 +8,8 @@ class HasPermission(BasePermission):
         self.permission_name = permission_name
 
     def has_permission(self, request, view):
-        # Check if the user has the specified permission
         return request.user.has_perm(self.permission_name) or request.user.is_superuser
     def has_object_permission(self, request, view, obj):
-        # Check if the user has the specified permission for the object
         return request.user.has_perm(self.permission_name, obj) or request.user.is_superuser
     def has_method_permission(self, request, view):
-        # Check if the user has the specified permission for the method
         return request.method in SAFE_METHODS or request.user.has_perm(self.permission_name) or request.user.is_superuser
